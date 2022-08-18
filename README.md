0abee76573925c25c241fb8f59453ca839632e96
Darren Saw <darrenskz@gmail.com>
2022-08-18 17:26:52 +1000

    added more dotfiles

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # dotfiles
 
+## Config Files for apps
+
 ### aria2
 `%USERPROFILE%\.aria2`
 
@@ -7,9 +9,17 @@
 `%USERPROFILE%\gallery-dl`
 
 ### neovim
-`%LOCALAPPDATA%`
+`%LOCALAPPDATA%\nvim`
+
+### yt-dlp
+`%APPDATA%\yt-dlp`
+
+## Shell Stuff
 
 ### CMD Prompt
 ```
 $E[0;31mdarre$E[0;37m@$E[0;33m%userdomain% $E[0;37min $E[0;32m$p $E[0;37m$b $t$h$h$h$_$$$s
 ```
+
+### Powershell
+`%USERPROFILE%\Documents\Powershell`
---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,118 @@
+# $executionTime = Measure-Command {}
+
+#$env:AZ_ENABLED=$false
+#oh-my-posh prompt init pwsh --config ~/mytheme.omp.json | Invoke-Expression
+
+function prompt {
+    # Write-Host $?
+    $check = $?
+    Write-Host ("$env:UserName") -ForegroundColor Red -NoNewline
+	Write-Host ("@") -NoNewline
+	Write-Host ($env:ComputerName).ToLower() -NoNewline -ForegroundColor Yellow
+	Write-Host (" in ") -NoNewline
+    Write-Host ("$(Get-Location)") -NoNewline -ForegroundColor Green
+    Write-Host (" | ") -NoNewline
+    Write-Host ("$(Get-Date -Format "HH:mm:ss")") -NoNewLine
+    if ($check) {
+        Write-Host ("")
+    }
+    else {
+        Write-Host (" err") -ForegroundColor Yellow
+    }
+	Write-Host ("$") -NoNewline
+    return " "
+}
+function which($name)
+{
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+function mergeAudio($video) {
+    if (Test-Path($video)) {
+        $output = (Get-Item $video).Name
+        ffmpeg -i $video -filter_complex "[0:a:0][0:a:1]amerge=inputs=2[outa]" -c:v copy -c:a aac -map 0:v -map "[outa]" "merged_$output"
+    } else {
+        "$VideoInput is Invalid"
+    }
+}
+
+function trim() {
+    [CmdletBinding(PositionalBinding=$False)]
+    Param(
+        [Parameter(Mandatory, Position=0)]
+        [Alias("i","input")]
+        [string]
+        $VideoInput,
+        
+        [Parameter(Mandatory)]
+        [Alias("s")]
+        [string]
+        $start,
+        
+        [Alias("d")]
+        [int]
+        $duration,
+
+        [Alias("vc")]
+        [string]
+        $vcodec = "hevc_nvenc",
+
+        [Alias("p")]
+        [string]
+        $preset = "slow",
+
+        [Alias("q")]
+        [int]
+        $qp = 23,
+        
+        [Parameter(Position=1)]
+        [Alias("o")]
+        [string]
+        $output
+    )
+    
+    if (Test-Path($VideoInput)) {
+        $video_name = (Get-Item $VideoInput).Basename
+        if ($duration -eq 0) {
+            if ($output -eq "") {
+                ffmpeg -hide_banner -ss $start -i "$VideoInput" -vcodec $vcodec -qp $qp -map 0 -preset $preset .\clips\trimmed_$video_name.mp4			
+            } else {
+                ffmpeg -hide_banner -ss $start -i "$VideoInput" -vcodec $vcodec -qp $qp -map 0 -preset $preset .\clips\$output.mp4
+            }
+        } else {
+            if ($output -eq "") {
+                ffmpeg -hide_banner -ss $start -t $duration -i "$VideoInput" -vcodec $vcodec -qp $qp -preset $preset -map 0 .\clips\trimmed_$video_name.mp4			
+            } else {
+                ffmpeg -hide_banner -ss $start -t $duration -i "$VideoInput" -vcodec $vcodec -qp $qp -preset $preset -map 0 .\clips\$output.mp4
+            }
+        }
+    } else {
+        "Video Path Invalid"
+    }
+}
+
+New-Alias grep Select-String
+
+New-Alias java8 "C:\Users\darre\.jdks\temurin-1.8.0_332\bin\java.exe"
+New-Alias java11 "C:\Users\darre\.jdks\temurin-11.0.15\bin\java.exe"
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
+function jpgq($a) {
+    $extn = [IO.Path]::GetExtension($a)
+    if ($extn -eq ".jpg") {
+        magick identify -verbose $a | grep quality	
+    }
+    elseif ($extn -eq ".jpeg") {
+        magick identify -verbose $a | grep quality
+    }
+    else {
+        "Not a jpg"
+    }
+}
+
+# Write-Host "$PSCommandPath execution time: $executionTime"
