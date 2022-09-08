94e737612844f0b4550d9e78aa04a87cfa0a56a0
Darren Saw <darrenskz@gmail.com>
2022-09-08 21:06:29 +1000

    improved things

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -7,9 +7,9 @@ function prompt {
     # Write-Host $?
     $check = $?
     Write-Host ("$env:UserName") -ForegroundColor Red -NoNewline
-	Write-Host ("@") -NoNewline
-	Write-Host ($env:ComputerName).ToLower() -NoNewline -ForegroundColor Yellow
-	Write-Host (" in ") -NoNewline
+    Write-Host ("@") -NoNewline
+    Write-Host ($env:ComputerName).ToLower() -NoNewline -ForegroundColor Yellow
+    Write-Host (" in ") -NoNewline
     Write-Host ("$(Get-Location)") -NoNewline -ForegroundColor Green
     Write-Host (" | ") -NoNewline
     Write-Host ("$(Get-Date -Format "HH:mm:ss")") -NoNewLine
@@ -19,16 +19,15 @@ function prompt {
     else {
         Write-Host (" err") -ForegroundColor Yellow
     }
-	Write-Host ("$") -NoNewline
+    Write-Host ("$") -NoNewline
     return " "
 }
-function which($name)
-{
+function which($name) {
     Get-Command $name | Select-Object -ExpandProperty Definition
 }
 
 function jpgtowebp() {
-	[CmdletBinding(PositionalBinding=$False)]
+    [CmdletBinding(PositionalBinding = $False)]
     Param(
         [Alias("q")]
         [int]
@@ -42,7 +41,7 @@ function jpgtowebp() {
         $resize = ""
     }
     # $output = "original"
-	New-Item -ItemType Directory -Force -Path .\$output | Out-Null
+    New-Item -ItemType Directory -Force -Path .\$output | Out-Null
     if ($resize -eq "") {
         if ($quality -eq -1) {
             magick mogrify -strip -format webp -identify *.jpg
@@ -57,7 +56,7 @@ function jpgtowebp() {
         }
     }
     # Get-Item *.jpg | Move-Item -Destination .\$output
-    Get-ChildItem *.jpg | ForEach-Object {Recycle-Item $_.FullName}
+    Get-ChildItem *.jpg | ForEach-Object { Recycle-Item $_.FullName }
 }
 
 function mergeAudio($video) {
@@ -70,10 +69,10 @@ function mergeAudio($video) {
 }
 
 function trim() {
-    [CmdletBinding(PositionalBinding=$False)]
+    [CmdletBinding(PositionalBinding = $False)]
     Param(
-        [Parameter(Mandatory, Position=0)]
-        [Alias("i","input")]
+        [Parameter(Mandatory, Position = 0)]
+        [Alias("i", "input")]
         [string]
         $VideoInput,
         
@@ -83,8 +82,8 @@ function trim() {
         $start,
         
         [Alias("d")]
-        [int]
-        $duration,
+        [string]
+        $duration = 0,
 
         [Alias("vc")]
         [string]
@@ -98,7 +97,7 @@ function trim() {
         [int]
         $qp = 23,
         
-        [Parameter(Position=1)]
+        [Parameter(Position = 1)]
         [Alias("o")]
         [string]
         $output
@@ -106,19 +105,15 @@ function trim() {
     
     if (Test-Path($VideoInput)) {
         $video_name = (Get-Item $VideoInput).Basename
-        if ($duration -eq 0) {
-            if ($output -eq "") {
-                ffmpeg -hide_banner -ss $start -i "$VideoInput" -vcodec $vcodec -qp $qp -map 0 -preset $preset .\clips\trimmed_$video_name.mp4			
-            } else {
-                ffmpeg -hide_banner -ss $start -i "$VideoInput" -vcodec $vcodec -qp $qp -map 0 -preset $preset .\clips\$output.mp4
-            }
-        } else {
-            if ($output -eq "") {
-                ffmpeg -hide_banner -ss $start -t $duration -i "$VideoInput" -vcodec $vcodec -qp $qp -preset $preset -map 0 .\clips\trimmed_$video_name.mp4			
-            } else {
-                ffmpeg -hide_banner -ss $start -t $duration -i "$VideoInput" -vcodec $vcodec -qp $qp -preset $preset -map 0 .\clips\$output.mp4
-            }
-        }
+        ffmpeg -hide_banner `
+            -ss $start `
+            -t $duration `
+            -i "$VideoInput" `
+            -vcodec $vcodec `
+            -qp $qp `
+            -preset $preset `
+            -map 0 `
+            $(If ($output -eq "") {".\clips\trimmed_$video_name.mp4"} Else {".\clips\$output"})
     } else {
         "Video Path Invalid"
     }
@@ -137,11 +132,8 @@ if (Test-Path($ChocolateyProfile)) {
 
 function jpgq($a) {
     $extn = [IO.Path]::GetExtension($a)
-    if ($extn -eq ".jpg") {
+    if ($extn -eq ".jpg" -or $extn -eq '.jpeg') {
         magick identify -verbose $a | grep quality	
-    }
-    elseif ($extn -eq ".jpeg") {
-        magick identify -verbose $a | grep quality
     }
     else {
         "Not a jpg"
@@ -154,8 +146,8 @@ function Recycle-Item {
     $shell = New-Object -ComObject 'Shell.Application'
 
     $shell.NameSpace(0).
-           ParseName($Path).
-           InvokeVerb('Delete')
+    ParseName($Path).
+    InvokeVerb('Delete')
 }
 
 # Write-Host "$PSCommandPath execution time: $executionTime"
